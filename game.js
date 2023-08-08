e7a8ee0e060b605b2f6dd73cd4791ba1261cc8db
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-08-08 11:36:31 +0530

    added hello world text

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 const scene = {
     preload: function() {
-
+        this.load.bitmapFont('arcade', 'assets/fonts/arcade.png', 'assets/fonts/arcade.xml');
     },
     create: function() {
-
+        this.helloText = this.add.bitmapText(400,300,'arcade', 'Hello World').setOrigin(0.5);
     },
     update: function() {
 
@@ -11,7 +11,20 @@ const scene = {
 }
 
 const config = {
-
+    type: Phaser.AUTO,
+    width: 80 * 16,
+    height: 50 * 16,
+    backgroundColor: '#000000',
+    parent: 'game',
+    pixelArt: true,
+    zoom: 1,
+    scene: scene,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 }
+        }
+    }
 }
 
 const game = new Phaser.Game(config)
