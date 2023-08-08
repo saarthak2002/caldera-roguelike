d82ccd9bca9a1becb4555f9492ca8f5a8407e68d
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-08-08 11:28:06 +0530

    added game.js

---
+++ game.js
@@ -0,0 +1,17 @@
+const scene = {
+    preload: function() {
+
+    },
+    create: function() {
+
+    },
+    update: function() {
+
+    }
+}
+
+const config = {
+
+}
+
+const game = new Phaser.Game(config)
