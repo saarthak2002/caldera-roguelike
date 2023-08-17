fd73249737bb51a9bfcf3d4b662c9fb1a93c2b70
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-08-17 17:35:59 +0530

    added data structure to represent rooms

--- bspdungeon.js
+++ bspdungeon.js
@@ -112,3 +112,14 @@ export default class BSPDungeon {
         return this.levelData;
     }
 }
+
+class DungeonRoom {
+    constructor(area) {
+        this.x = Math.floor(area.x +(Phaser.Math.Between(1, area.w) / 3));
+        this.y = Math.floor(area.y + (Phaser.Math.Between(1, area.h) / 3));
+        this.w = area.w - (this.x - area.x);
+        this.h = area.h - (this.y - area.y);
+        this.w -= Math.floor(Phaser.Math.Between(1, this.w / 3));
+        this.h -= Math.floor(Phaser.Math.Between(1, this.h / 3));
+    }
+}
